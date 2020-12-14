f063b885868b62fa618be1413ece138480ba44ce
Zain Fareed <zainfareed093@gmail.com>
2020-12-13 21:23:45 -0800

    Completed

--- app/src/main/java/com/example/tabsapplication/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/tabsapplication/ui/main/SectionsPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.tabsapplication.ui.main;
 
 import android.content.Context;
+import android.widget.Switch;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
@@ -8,39 +9,49 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import com.example.tabsapplication.Daily;
 import com.example.tabsapplication.R;
+import com.example.tabsapplication.Today;
+import com.example.tabsapplication.Weekly;
 
-/**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to
- * one of the sections/tabs/pages.
- */
+
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-    @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
-    private final Context mContext;
+    int numberOfTabs;
+    private static final String[] TAB_TITLES = new String[]{"Today","Daily","Weekly"};
 
-    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+
+    public SectionsPagerAdapter(FragmentManager fm, int numberOfTabs) {
         super(fm);
-        mContext = context;
+        this.numberOfTabs = numberOfTabs;
     }
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new Today();
+                break;
+            case 1:
+                fragment = new Daily();
+                break;
+            case 2:
+                fragment = new Weekly();
+                break;
+        }
+        return fragment;
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        return mContext.getResources().getString(TAB_TITLES[position]);
+        return TAB_TITLES[position];
     }
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+
+        return numberOfTabs;
     }
 }
